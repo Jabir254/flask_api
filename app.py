3d2d34f2beba7ab26eba224020ed018bb499936f
jabir254 <jabirkullow@gmail.com>
2023-11-10 20:34:40 +0300

    added homepage

--- app.py
+++ app.py
@@ -2,13 +2,5 @@
 from flask import Flask
 from config import *
 
-
-
-@app.route("/")
-def homepage():
-    return "hello world"
-
-
-
 if __name__ == "__main__":
-    app.run(debug = True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
---
+++ routes.py
@@ -0,0 +1,6 @@
+from app import *
+from flask import render_template
+
+@app.route("/")
+def homepage():
+    return render_template('index.html')
