4b0c3cf1a72b98a3bdec2dafa07465e59c46e2e4
jabir254 <jabirkullow@gmail.com>
2023-11-10 21:33:52 +0300

    updated

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 """Contains all the routes"""
-from flask import Flask
+from flask import Flask, jsonify, redirect, render_template, request
 
 
 app = Flask(__name__)
 
+@app.route("/")
+def homepage():
+    return render_template('index.html')
+
+
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000,)
--- routes.py
+++ routes.py
@@ -1,7 +1,1 @@
-from app import *
-from flask import render_template
 
-
-@app.route("/")
-def homepage():
-    return render_template('index.html')
