2e5129c5086ad784fcd2b91217e3fd89a3822c57
jabir254 <jabirkullow@gmail.com>
2023-11-11 20:58:36 +0300

    added model

---
+++ Backend/models/models.py
@@ -0,0 +1,11 @@
+from app import db
+
+
+class Employee(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(64), index=True)
+    lastName = db.Column(db.String(64), index=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+
+    def __repr__(self) -> str:
+        return '<Employee {}>'.format(self.firstName)
