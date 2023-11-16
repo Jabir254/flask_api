423d663ab0ce967c84f131689943c7113d21acef
jabir254 <jabirkullow@gmail.com>
2023-11-16 11:28:01 +0300

    fixed import app error

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -6,12 +6,7 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'employee.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -19,16 +14,13 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db, render_as_batch=True)
 
 from model.models import Role, Employee, User, Attendance, Leave, Position
+from app.routes import Route
 
 @app.shell_context_processor
 def make_shell_context():
     return {"db": db, "Role": Role, Employee: "Employee", User: "User",
             Attendance: "Attendance", Leave: "Leave", Position: "Position"}
 
-
-@app.route('/')
-def index():
-    return 'Hello world'
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Backend/app/routes.py
+++ Backend/app/routes.py
@@ -1,4 +1,8 @@
-from main import app
 from flask import jsonify, abort
 from model.models import Employee, Role
+from app import app
 
+class Route():
+    @app.route("/")
+    def index():
+        return"hello world"
