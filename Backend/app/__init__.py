c7f2a54cd788ee51941013fb6a903f7246e90f18
jabir254 <jabirkullow@gmail.com>
2023-11-16 11:18:36 +0300

    updated

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -1,15 +1,23 @@
 
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'employee.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db, render_as_batch=True)
 
-from app.routes import route
 from model.models import Role, Employee, User, Attendance, Leave, Position
 
 @app.shell_context_processor
@@ -17,6 +25,10 @@ def make_shell_context():
     return {"db": db, "Role": Role, Employee: "Employee", User: "User",
             Attendance: "Attendance", Leave: "Leave", Position: "Position"}
 
+
+@app.route('/')
+def index():
+    return 'Hello world'
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Backend/app/routes.py
+++ Backend/app/routes.py
@@ -2,6 +2,3 @@ from main import app
 from flask import jsonify, abort
 from model.models import Employee, Role
 
-@app.route('/')
-def index():
-    return 'Hello world'
